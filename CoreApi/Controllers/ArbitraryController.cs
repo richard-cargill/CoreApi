2f0cdc8a528f446a4a79fe547b3d27878a55bfe2
Richard Cargill <catdoce@gmail.com>
2018-01-30 16:18:13 +0000

    inital commit

---
+++ CoreApi/Controllers/ArbitraryController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreApi.Models;
+
+namespace CoreApi.Controllers
+{
+    [Route("api/data")]
+    public class ArbitraryController : Controller
+    {
+        [HttpGet()]
+        public IActionResult GetData()
+        {
+            return Ok(DataStore.Current.ArbitaryModelList);
+        }
+
+        [HttpGet("{id}", Name = "GetData")]
+        public IActionResult GetData(int id)
+        {
+            var dataToReturn = DataStore.Current.ArbitaryModelList.FirstOrDefault(d => d.Id == id);
+
+            if (dataToReturn == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(dataToReturn);
+        }
+
+        [HttpPost()]
+        public IActionResult CreateData([FromBody] ArbitraryModel arbitraryModel)
+        {
+            if (arbitraryModel == null)
+            {
+                return BadRequest();
+            }
+
+            var maxDataId = DataStore.Current.ArbitaryModelList.Max(d => d.Id);
+
+            var finalData = new ArbitraryModel()
+            {
+                Id = ++maxDataId,
+                Name = arbitraryModel.Name,
+                Description = arbitraryModel.Description
+            };
+
+            DataStore.Current.ArbitaryModelList.Add(finalData);
+
+            return CreatedAtRoute("GetData", new
+            { id = finalData.Id }, finalData);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateData(int id, [FromBody] ArbitraryModel arbitraryModel)
+        {
+            if (arbitraryModel == null)
+            {
+                return BadRequest();
+            }
+
+            var dataToUpdate = DataStore.Current.ArbitaryModelList.FirstOrDefault(d => d.Id == id);
+
+            if (dataToUpdate == null)
+            {
+                return NotFound();
+            }
+
+            dataToUpdate.Name = arbitraryModel.Name;
+            dataToUpdate.Description = arbitraryModel.Description;
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteData(int id, [FromBody] ArbitraryModel arbitraryModel)
+        {
+            if (arbitraryModel == null)
+            {
+                return BadRequest();
+            }
+
+            var dataToUpdate = DataStore.Current.ArbitaryModelList.FirstOrDefault(d => d.Id == id);
+
+            if (dataToUpdate == null)
+            {
+                return NotFound();
+            }
+
+            DataStore.Current.ArbitaryModelList.Remove(dataToUpdate);
+
+            return NoContent();
+        }
+    }
+}
---
+++ CoreApi/DataStore.cs
@@ -0,0 +1,39 @@
+﻿using CoreApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreApi
+{
+    public class DataStore
+    {
+        public static DataStore Current { get; } = new DataStore();
+        public List<ArbitraryModel> ArbitaryModelList { get; set; }
+
+        public DataStore()
+        {
+            ArbitaryModelList = new List<ArbitraryModel>()
+            {
+                new ArbitraryModel()
+                {
+                    Id = 1,
+                    Name = "Test 1",
+                    Description = "Another description of stuff"
+                },
+                new ArbitraryModel()
+                {
+                    Id = 2,
+                    Name = "Test 2",
+                    Description = "Another description of stuff"
+                },
+                new ArbitraryModel()
+                {
+                    Id = 3,
+                    Name = "Test 3",
+                    Description = "Another description of stuff"
+                }
+            };
+        }
+    }
+}
